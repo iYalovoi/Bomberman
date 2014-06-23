6060b04e702c444953f68931965f0bf0db4815ff
Aleksey <invalidreference@hotmail.com>
2014-06-23 07:57:02 +0400

    Onil behaviour adjusted

--- Assets/Script/Onil.cs
+++ Assets/Script/Onil.cs
@@ -8,12 +8,50 @@ namespace Assets.Script
     {
         public Onil()
         {
-            MaxSpeed = 2f;
+            MaxSpeed = 1f;
         }
 
         private void FixedUpdate()
         {
-
+			if(!Dead)
+			{
+				//Kind of weird way to determine tile size?
+				var tileSize = renderer.bounds.size.x;
+				//Local position seems to be bottom left corner?
+				var localPosition = gameObject.transform.localPosition;
+				//
+				var currentTilePosition = new Vector2(tileSize * Mathf.Round(localPosition.x / tileSize), tileSize * Mathf.Round(localPosition.y / tileSize));
+				const float eps = 0.1f;
+				Direction way = Direction.Undefined;
+				//current position is close to the bottom left corner of the tile?
+				if ((Mathf.Abs(localPosition.x - currentTilePosition.x) < eps) && (Mathf.Abs(localPosition.y - currentTilePosition.y) < eps) )
+				{
+					//Change direction with 10% probability
+					if(Random.value > 0.9)
+					{
+						var randomWay = (Direction)Mathf.Pow(2, Random.Range(0, 4));
+						var block = MapDiscovery.BlastInDirection(transform.position, tileSize, randomWay, 1);
+						//do we check against bomb here as well?
+						//If the way is blocked we might sit here for a while until proper direction is randomed?
+						if (block.Select(o => o.transform.gameObject).All(o => (o.tag != "Wall")))
+							way = randomWay;   
+					}                 
+				}
+				//Only if we change direction
+				if(way != Direction.Undefined)
+				{
+					rigidbody2D.velocity = MaxSpeed * way.ToVector2();
+					if(way == Direction.Left)
+					{
+						animator.SetFloat("Direction", 1);
+					}
+					if(way == Direction.Right)
+					{
+						animator.SetFloat("Direction", -1);
+					}
+				}
+			}
+			else rigidbody2D.velocity = new Vector2();;
         }
 
     }
--- Assets/Script/Utility/Direction.cs
+++ Assets/Script/Utility/Direction.cs
@@ -15,4 +15,16 @@ namespace Assets.Script.Utility
         Vertical = Up | Down,
         Horizontal = Left | Right,
     }
+
+	public static class DirectionExtensions
+	{
+		public static Vector2 ToVector2(this Direction direction)
+		{
+			var result = new Vector2 ();
+			result.x = (direction.IsFlagSet(Direction.Right)?1:0) - (direction.IsFlagSet(Direction.Left)?1:0);
+			result.y = (direction.IsFlagSet(Direction.Up)?1:0) - (direction.IsFlagSet(Direction.Down)?1:0);
+			return result;
+		}
+
+	}
 }
