496022575012484506f6adf9c475cbf69d085dc6
Igor Yalovoy <igor@yalovoi.net>
2015-03-09 20:46:00 +0700

    Hint pictures

--- Assets/ReBomber/Script/Render/LevelSplash.cs
+++ Assets/ReBomber/Script/Render/LevelSplash.cs
@@ -2,6 +2,7 @@
 using Assets.Script.Utility;
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Assets.Script
 {
@@ -12,29 +13,83 @@ namespace Assets.Script
         private Messenger _messenger;
         private TextMeshPro _textMeshPro;
 
+        public UI2DSprite Hint;
+
+        public Sprite Controls;
+        public Sprite Flames;
+        public Sprite Bombs;
+        public Sprite BombsPass;
+        public Sprite Detonator;
+        public Sprite FlamePass;
+        public Sprite Mystery;
+        public Sprite Speed;
+        public Sprite WallPass;
+
         protected override void Start()
         {
             base.Start();
-            StartCoroutine(WaitAndLoadNewLevel());
         }
 
-        private IEnumerator WaitAndLoadNewLevel()
+        void Update()
         {
-            yield return new WaitForSeconds(3);
-            _messenger.Signal(Signals.LoadNextLevel);
+            if (Input.anyKey)
+                _messenger.Signal(Signals.LoadNextLevel);
         }
 
         private void OnInjected(Messenger messenger, GameModel model)
         {
+            var sprites = new List<Sprite>
+            {
+                Controls,
+                Flames,
+                Bombs,
+                BombsPass,
+                Detonator,
+                FlamePass,
+                Mystery,
+                Speed,
+                WallPass,
+            };
+
             _model = model;
             _messenger = messenger;
 
             _textMeshPro = GetComponent<TextMeshPro>();
             _textMeshPro.text = string.Format("Level {0}", _model.CurrentLevel);
-        }
 
-        void Update()
-        {
+            switch (_model.CurrentLevel)
+            {
+                case 1:
+                    Hint.sprite2D = Controls;
+                    break;
+                case 2:
+                    Hint.sprite2D = Bombs;
+                    break;
+                case 3:
+                    Hint.sprite2D = Detonator;
+                    break;
+                case 4:
+                    Hint.sprite2D = Speed;
+                    break;
+                case 7:
+                    Hint.sprite2D = Flames;
+                    break;
+                case 9:
+                    Hint.sprite2D = BombsPass;
+                    break;
+                case 10:
+                    Hint.sprite2D = WallPass;
+                    break;
+                case 26:
+                    Hint.sprite2D = Mystery;
+                    break;
+                case 30:
+                    Hint.sprite2D = FlamePass;
+                    break;
+                default:
+                    Hint.sprite2D = sprites[Random.Range(0, sprites.Count)];
+                    break;
+            }
         }
     }
 }
